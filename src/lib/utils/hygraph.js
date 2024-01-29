d588be3ec134858ead63263e96d6760ac501d144
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-01-29 21:46:57 +0100

    .

--- src/lib/utils/hygraph.js
+++ src/lib/utils/hygraph.js
@@ -1,9 +1,21 @@
+// import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private'
+//
+// import { GraphQLClient } from 'graphql-request'
+//
+// export const hygraph = new GraphQLClient(HYGRAPH_URL, {
+//   headers: {
+//     Authorization: `Bearer ${HYGRAPH_KEY}`,
+//   },
+// })
+
 import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private'
 
-import { GraphQLClient } from 'graphql-request'
+import { GraphQLClient } from 'graphql-request';
+
+console.log(HYGRAPH_URL)
 
 export const hygraph = new GraphQLClient(HYGRAPH_URL, {
   headers: {
-    Authorization: `Bearer ${HYGRAPH_KEY}`,
-  },
-})
+    Authorization: `Bearer ${HYGRAPH_KEY}`
+  }
+});
