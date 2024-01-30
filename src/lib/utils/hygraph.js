b8b1419dfe997ea3cb5761469cdb92e228c1783b
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-01-30 21:52:26 +0100

    Update hygraph.js

--- src/lib/utils/hygraph.js
+++ src/lib/utils/hygraph.js
@@ -1,7 +1,7 @@
 import {
   HYGRAPH_URL,
   HYGRAPH_KEY
-} from '.env/static/private';
+} from '$env/static/private';
 import { GraphQLClient } from 'graphql-request'
 
 export const hygraph = new GraphQLClient(HYGRAPH_URL, {
