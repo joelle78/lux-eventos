6e814305b24f07e97cd71cb5a03e35d3c1285c2e
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-01-30 21:34:44 +0100

    .

--- src/lib/utils/hygraph.js
+++ src/lib/utils/hygraph.js
@@ -1,7 +1,7 @@
 import {
   HYGRAPH_URL,
   HYGRAPH_KEY
-} from '$env/static/private';
+} from '$env/dynamic/private';
 import { GraphQLClient } from 'graphql-request'
 
 export const hygraph = new GraphQLClient(HYGRAPH_URL, {
