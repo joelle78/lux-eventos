5ac4edd1389d04e1d9e04a8d4ca4d05ba625be92
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-01-31 15:59:10 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-// import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vite';
-//
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
-
-
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-			external: ['$env/static/private'],
-		},
-	},
+	plugins: [sveltekit()]
 });
+
+
